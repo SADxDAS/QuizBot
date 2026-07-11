ac73d957d4410898ec9fb0c225103cdedfc9aa19
SADxDAS <oleg.sherkas05@gmail.com>
2026-07-11 14:11:09 +0300

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -3,7 +3,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 def get_admin_keyboard():
     builder = ReplyKeyboardBuilder()
     builder.button(text="📊 Переглянути відповіді")
-    builder.button(text="⚙️ Список питань")
+    builder.button(text="📃 Список питань")
     builder.button(text="➕ Створити питання")
     builder.button(text="🚀 Запуск опитування")
     builder.adjust(2)
